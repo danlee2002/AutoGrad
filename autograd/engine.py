da0327352b905febdb0dc7efb52fbc0cc00d63f0
danlee2002 <68606021+danlee2002@users.noreply.github.com>
2024-01-28 17:49:07 -0500

    fixed bug regarding reverse mode

--- autograd/engine.py
+++ autograd/engine.py
@@ -1,129 +1,103 @@
-import numpy as np 
-import math
+import numpy as np
 from typing import Union, Tuple
-
 class Tensor:
-    """
-    Initializes Tensor class from np.ndarray object or list
-    """
-    def __init__(self, data: Union[int, float, np.ndarray, list], _children: Tuple[type["Tensor"]] = (), _op = '', label:str = ''):
-        if isinstance(data, np.ndarray, ):
-            self.data = data
-        else: 
-            self.data = np.array(data)
-        #initalizes gradient
-        self.grad = np.zeros_like(self.data)
-        self._backward = lambda: None
-        #sets nodes of parents 
-        self.prev = set(_children)
-        #operation associated with current nodes
-        self._op = _op
-        #label for visualization 
-        self.label = label 
+  
+  def __init__(self, data: Union[int, float, np.ndarray, list], _children:tuple[type["Tensor"]]=(), _op='', label='') -> type["Tensor"]:
+    if isinstance(data, np.ndarray):
+      self.data = data
+    else:
+      self.data = np.array(data)
+    self.grad = np.zeros_like(self.data)
+    self._backward = lambda: None
+    self._prev = set(_children)
+    self._op = _op
+    self.label = label
 
-    def __repr__(self):
-        return f'Value: {self.data}'
+  def __add__(self, other: Union[int, float, np.ndarray, list]) -> type["Tensor"]:
+    other = self.checktype(other)
+    out = Tensor(self.data + other.data, (self, other), '+')
+    def _backward():
+      self.grad = self.grad + (1.0 * out.grad)
+      other.grad = other.grad + (1.0 * out.grad)
+    out._backward = _backward
+    return out
 
-    def __truediv__(self, other: Union[int, float, np.ndarray, type["Tensor"]]) -> type["Tensor"]:
-        ...
+  def __mul__(self, other: Union[int, float, np.ndarray, list]) -> type["Tensor"]:
+    other = self.checktype(other)
+    out = Tensor(self.data * other.data, (self, other), '*')
+    def _backward():
+      self.grad = self.grad + (other.data * out.grad)
+      other.grad = other.grad + (self.data * out.grad)
+    out._backward = _backward
+    return out
 
-    def __add__(self, other:Union[int, float, np.ndarray, type["Tensor"]]) -> type["Tensor"]:
-        other = self.checktype(other)
-        out = Tensor(data = self.data + other.data, _children = (self,other), _op = '+')
-        def _backward():
-            self.grad = self.grad + (1.0 * out.grad)
-            other.grad = other.grad + (1.0 * out.grad)
-        out._backward = _backward
-        return out
-    
-    def __radd__(self, other):
-        return self + other
+  def __pow__(self, other: Union[int, float, np.ndarray, list]) -> type["Tensor"]:
+    if isinstance(other, (int, float)):
+      out_data = np.power(self.data, other)
+      out = Tensor(out_data, (self,), f'**{other}')
+      def _backward():
+        self.grad = self.grad + ((other * np.power(self.data, other - 1)) * out.grad)
 
-    def __mul__(self, other: Union[int, float, np.ndarray, type["Tensor"]]) ->  type["Tensor"]:
-        other = self.checktype(other)
-        out = Tensor(data = self.data * other.data, _children = (self, other), _op = '*')
-        def _backward():
-            self.grad = self.grad + (other.data * out.grad)
-            other.grad = other.grad + (self.data * out.grad)
-        out._backward = _backward
-        return out 
-   
-   
-    def __matmul__(self, other: Union[int, float, np.ndarray, type["Tensor"]]) -> type["Tensor"]:
-        other =  self.checktype(other)
-        ...
-    
+      out._backward = _backward
+      return out
+    else:
+      out_data = np.power(self.data, other.data)
+      out = Tensor(out_data, (self, other), f'**')
+      def _backward():
+        self.grad = self.grad + ((other.data * np.power(self.data * other.data - 1)) * out.grad)
+        other.grad = other.grad + (np.log(self.data) * out.grad)
+      out._backward = _backward
+      return out
+ 
+  def __radd__(self, other: Union[int, float, np.ndarray, list]) -> type["Tensor"]:
+    return self + other
 
-    def __pow__(self, other: Union[int, float, np.ndarray, type["Tensor"]])  -> type["Tensor"]:
-        # handles scalar case 
-        if isinstance(other, (int, float)):
-            out = Tensor(data =self.data**other, _children= (self,), _op = '**')
-            def _backward():
-                self.grad = self.grad + (other * np.power(self.data, other - 1) * out.grad)
-            out._backward = _backward
-            return out 
-        else:
-            other = self.checktype(other)
-            out = Tensor(data = np.power(self.data,other.data),_children= (self, other), _op = '**')
-            def _backward():
-                self.grad = self.grad + (other.data * np.power(self.data, other.data - 1) * out.grad)
-                other.grad= other.grad + (np.log(self.data) * out.grad)
-            out._backward = _backward
-            return out 
+  def __rmul__(self, other: Union[int, float, np.ndarray, list]) -> type["Tensor"]:
+    return self * other
 
-    def __neg__(self) -> type["Tensor"]:
-        return self * -1
+  def __truediv__(self, other: Union[int, float, np.ndarray, list]) -> type["Tensor"]:
+    return self * other**-1
 
-    def __sub__(self, other: Union[float, int, np.ndarray,type["Tensor"]]) ->type["Tensor"] :
-        return self + (-other)
+  def __neg__(self) -> type["Tensor"]:
+    return self * -1
 
-    def __truediv__(self, other: Union[float, int, np.ndarray,type["Tensor"]]) -> type["Tensor"]:
-        return self * other ** -1
+  def __sub__(self, other: Union[int, float, np.ndarray, list]) -> type["Tensor"]:
+    return self + (-other)
 
-    def relu(self):
-        out = Tensor(data = np.maximum(self.data,0), _children = (self,), _op = "relu")
-        def _backward():
-            self.grad = self.grad + np.where(self.data >= 0, 1, 0)*out.grad
-        self._backward = _backward
-        return out
+  def exp(self) -> type["Tensor"]:
+    x = self.data
+    out = Tensor(np.exp(x), (self, ), 'exp')
+    def _backward():
+        self.grad = self.grad + (out.data * out.grad)
+    out._backward = _backward
+    return out
 
-    def tanh(self):
-        value = np.tanh(self.data)
-        out = Tensor(data = value, _children = (self,), _op = "tanh")
-        def _backward():
-            self.grad = self.grad + (1 - value**2) * out.grad
-        self._backward = _backward
-        return out 
-    
-    def sigmoid(self):
-        sgmd = 1/(1 + np.exp(self.data))
-        out = Tensor(data = sgmd, _children = (self,),_op = "sigmoid")
-        def _backward():
-            self.grad = self.grad + (1 - sgmd)* sgmd
-        self._backward = _backward
-        return out 
-    
-        
+  def tanh(self) -> type["Tensor"]:
+    x = self.data
+    t = np.tanh(x)
+    out = Tensor(t, (self, ), 'tanh')
+    def _backward():
+        self.grad = self.grad + ((1 - t**2) * out.grad)
+    out._backward = _backward
+    return out
 
-    def checktype(self, other: any):
-        return other if isinstance(other,Tensor) else Tensor(other) 
-    
-
-
-
-
-    def backward(self):
-        topo = []
-        visited = set()
-        def topoSort(v):
-            if v not in visited:
-                visited.add(v)
-                for child in v.prev:
-                    topoSort(child)
+  def backward(self):
+    topo = []
+    visited = set()
+    def toposort(v):
+        if v not in visited:
+            visited.add(v)
+            for child in v._prev:
+                toposort(child)
             topo.append(v)
-       
-        topoSort(self)
-        self.grad = np.ones_like(self.data)
-        for nodes in reversed(topo):
-            nodes._backward()
+    toposort(self)
+    self.grad = np.ones_like(self.data)
+    for node in reversed(topo):
+        node._backward()
 
+  def __repr__(self):
+    return f"Value(data={self.data})"
+  
+  # checks if input is Tensor and converts otherwise
+  def checktype(self, other: Union[int, float, np.ndarray, list]) -> type["Tensor"]:
+    return other if isinstance(other, Tensor) else Tensor(other)
