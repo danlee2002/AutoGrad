33059455b16e3b34008554f756d51aadc346b2fe
danlee2002 <68606021+danlee2002@users.noreply.github.com>
2024-02-02 01:04:30 -0500

    added requirements.txt

--- tests/halfspace.py
+++ tests/halfspace.py
@@ -6,7 +6,6 @@ from autograd import neuralnetwork as neural
 def main():
   model =neural.nn([neural.MLP(2,4,10,2)], loss = neural.nn.crossentropy, lr = 0.2)
   #sample program to learn a halfspace
-
   xs = [[-1,-2],[-1,-1],[1,3],[1,1],[2,1],[-3,-1],[-2,-2]]
   ys = [[1,0],[1,0],[0,1],[0,1],[1,0],[1,0]]
   for i in range(400):
